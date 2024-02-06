917f42f5bbad4aee63499622ddbe3a067540ed28
Gelzieny R. Martins <gelzieny@gmail.com>
2024-02-06 21:02:49 +0000

    Criando novos arquivos

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# analyze_dashboard
+# Analyze Dashboard
+
+------------------------------------------
+
+
+ENV 
+======
+
+* Linux / MAC
+
+```bash
+python -m venv venv && source venv/bin/activate && pip install -r requirements.txt
+```
+
+* Windows
+
+```bash
+python -m venv venv && venv\Scripts\activate.bat && pip install -r requirements.txt 
+```
---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np 
+import yfinance as yf 
+import plotly.express as px
+from  datetime import datetime
+from streamlit_extras.metric_cards import style_metric_cards
+from streamlit_extras.grid import grid
+
+st.title('Dashboard para Investidores')
+
+def build_sidebar():
+  pass
+
+def build_main():
+  pass
+
+with st.sidebar:
+  build_sidebar()
